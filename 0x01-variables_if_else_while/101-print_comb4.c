8cba2a9fad9efacc7647bb8b27c0015fd69451d0
TechkidErima <kevohbarasa@gmail.com>
2022-09-09 14:22:48 -0700

    kelvin

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,7 +10,6 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-
 		for (p = 1; p <= 9; p++)
 		{
 			for (m = 2; m <= 9; m++)
@@ -28,6 +27,7 @@ int main(void)
 				}
 			}	
 		}
+	}
 	putchar('\n');
 	return (0);
 }
