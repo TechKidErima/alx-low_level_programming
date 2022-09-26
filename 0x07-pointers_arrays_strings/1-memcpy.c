39368584946422aa54775087ca15c9be4c44d8ca
TechkidErima <kevohbarasa@gmail.com>
2022-09-26 00:03:26 -0700

    more arrays task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,9 +6,17 @@
  * char *_memcpy - copies memory area.
  * @dest - destination of copied memeory location
  * @src - source of memeory o\location
- *
+ * @n: length of buffer
  * Return: 0
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	_putchar
+	unsigned int i = 0;
+
+	while(i<n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return(dest);
+}
