5c7de7db266e8c8f3a2f4cb1fe59dcc8b35dad27
TechkidErima <kevohbarasa@gmail.com>
2022-09-26 00:14:42 -0700

    more pointers task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strchr -  locates a character in a string.
+ * @s: string to search
+ * @c: character t search
+ * Return: pointer to first occcurence of the 
+ */
+
+char *_strchr(char *s, char c)
+{
+	while(*s != '\0')
+	{
+		if(*s == c)
+		{
+			return(s);
+		}
+		++s;
+	}
+	if(*s == c)
+	{
+		return(s);
+	}
+
+	return(0);
+}
