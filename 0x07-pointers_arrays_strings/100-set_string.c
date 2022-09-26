20b4090126ef42d4696d4bffd8b8da335a7c9743
TechkidErima <kevohbarasa@gmail.com>
2022-09-26 01:17:25 -0700

    more pointers task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - set value of pointer to a char
+ * @s: pointer t pointer
+ * @to: ointer to char
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
