13afa9674472fea1513f04794ebe74b5cda98dd2
TechkidErima <kevohbarasa@gmail.com>
2022-09-27 00:06:54 -0700

    kelvi correction

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@
  * Desc: Header file containing prototypes
  */
 
+#include <stddef.h>
 int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
