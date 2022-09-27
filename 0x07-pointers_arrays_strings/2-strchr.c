839cf15808ea97f868e479cff8bd9ccd07983ec9
TechkidErima <kevohbarasa@gmail.com>
2022-09-27 01:00:00 -0700

    correction

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,13 +8,16 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != 0)
+	if (s == NULL)
+		return (NULL);
+	for (; *s; s++)
 	{
 		if (*s == c)
 			return (s);
-		s++;
 	}
 	if (*s == c)
+	{
 		return (s);
-	return (0);
+	}
+	return (NULL);
 }
