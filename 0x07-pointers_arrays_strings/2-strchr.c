9f24af277df246aaa8a273479452ee71b88b5840
TechkidErima <kevohbarasa@gmail.com>
2022-09-27 01:33:18 -0700

    correction

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,27 +1,24 @@
 #include "main.h"
+#include <stddef.h>
 /**
- * _strspn - gets the length of a prefix substring
- * @s: the string we look at
- * @accept: the string we compare to
+ *  _strchr - locates a character
+ *  @s: the string
+ *  @c: the character
  *
- * Return: unsigned int
+ *  Return: a pointer to the char
  */
-unsigned int _strspn(char *s, char *accept)
+char *_strchr(char *s, char c)
 {
-	int i = 0;
-	int j = 0;
-
-	for (; s[i]; i++)
+	if (s == NULL)
+		return (NULL);
+	for (; *s; s++)
 	{
-		for (j = 0; accept[j]; j++)
-		{
-			if (s[i] == accept[j])
-			{
-				break;
-			}
-		}
-		if (s[i] != accept[j])
-			break;
+		if (*s == c)
+			return (s);
 	}
-	return (i);
+	if (*s == c)
+	{
+		return (s);
+	}
+	return (NULL);
 }
