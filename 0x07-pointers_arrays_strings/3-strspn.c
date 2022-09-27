eed9b9a75a0c885c58f8b02f77f1c418e135e1a8
TechkidErima <kevohbarasa@gmail.com>
2022-09-27 00:56:26 -0700

    correction

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,24 +7,20 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	  unsigned int bytes = 0;
-	  int index;
+	int i = 0;
+	int j = 0;
 
-	   while (*s)
-	   {
-		   for (index = 0; accept[index]; index++)
-		   {
-			   if (*s == accept[index]) 
-			   {
-				   bytes++;
-				   break;
-			   }
-			    else if (accept[index + 1] == '\0') 
-				     return (bytes); 
-		   }
-		   s++; 
-	   }
-
-	  return (bytes);
+	for (; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				break;
+			}
+		}
+		if (s[i] != accept[j])
+			break;
+	}
+	return (i);
 }
-	   
