a8a02ce7d204a5e3c5e8179b35e1315f0851f3fc
TechkidErima <kevohbarasa@gmail.com>
2022-09-28 00:13:01 -0700

    recursion task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string to print
+ *
+ * Return: 0;
+ */
+void _puts_recursion(char *s)
+{
+	_putchar("%c\n", s);
+
+	return (0);
+}
