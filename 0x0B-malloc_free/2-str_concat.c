029447f8be194369a062cee6c957e2851dd2ed0c
TechkidErima <kevohbarasa@gmail.com>
2022-10-03 06:34:43 -0700

    memory allocation task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,6 +11,38 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *s1;
-	char *s2;
+	unsigned int int1, int2, size, i, j;
+	char *str;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	int1 = 0;
+	while (s1[len] != '\0')
+		int1++;
+	int2 = 0;
+	while (s2[int2] != '\0')
+		int2++;
+
+	size = int1 + int2;
+
+	str = malloc(size * sizeof(char) + 1);
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (i < int1)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (i <= size)
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	return (str);
 }
