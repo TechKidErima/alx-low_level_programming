69e2b21cc01ed38e319f9faed1b42a381dd74c6f
TechkidErima <kevohbarasa@gmail.com>
2022-10-03 21:29:47 -0700

    memory allocation task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - frees a 2 dimensional grid previously created
+ * @grid: grid
+ * @height: height
+ *
+ * Return: void
+ *
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	freee(grid);
+}
